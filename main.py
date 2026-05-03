059b3261625b627c3c4c288bab4adc24e042bf4f
Lutbe <dkotel82@gmail.com>
2026-05-03 13:19:49 +0300

    Add number guessing game to main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from random import*
 Ima = input('Введіть ваше імя')
 vik = input('Введіть ваш вік')
 print(f'Привіт {Ima}, тобі {vik} ')
+in1 = input('Тепер вгадай число від 1 до 10')
+in2 = randint(1, 10)
+if in1 == in2 :
+    print('вгадав')
+else :
+    print(f'не вгадав твоє число:{in2}')
